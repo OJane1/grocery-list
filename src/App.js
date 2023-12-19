447ce7e2c7ac6f9223ea3f51ea227b69949ab9ab
OJane1 <o.s.petrashova@gmail.com>
2023-12-19 11:01:52 +0100

    added grocery list app

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import image from './shopping.jpeg';
+import imageTwo from './man.jpeg';
+import { GroceryList } from './GroceryList';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container'>
+     <img src= { image } width="200px" alt="Shopping bag"/>
+     </div>
+     <div className='container'>
+     <h1>Grocery List</h1>
+     </div>
+     <GroceryList/>
+     <div className='container'>
+     <img src={ imageTwo } width="200px" alt="Shopping man"/>
+     </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/GroceryList.js
@@ -0,0 +1,70 @@
+import { Component } from 'react';
+import check from './icons8-check-48.png';
+
+export class GroceryList extends Component {
+    state = {
+        userInput: "",
+        groceryList: []
+    }
+    
+    onChangeEvent(e) {
+        this.setState({userInput: e});
+    }
+
+    addItem(input) {
+        if (input === '') {
+            alert("Please enter an item");
+        }
+        else {
+            let listArray = this.state.groceryList;
+            console.log(listArray);
+            listArray.push(input)
+            this.setState({groceryList: listArray, userInput: ''})
+        }  
+    }
+
+    deleteItem() {
+        let listArray = this.state.groceryList;
+        listArray = [];
+        this.setState({groceryList: listArray})
+    }
+
+    crossedWord(event) {
+        const li = event.target;
+        li.classList.toggle('crossed');
+    }
+
+    onFormSubmit(e) {
+        e.preventDefault();
+    }
+
+    render () {
+        return (
+            <div>
+                <form onSubmit={this.onFormSubmit}>
+            <div className='container'>
+                <input 
+                type="text"
+                placeholder="What would you like to buy today?"
+                onChange={(e) => {this.onChangeEvent(e.target.value)}}
+                value={this.state.userInput} />
+            </div>
+            <div className='container'>
+                <button onClick = {() => this.addItem(this.state.userInput)} className="btn btn-add">Add</button>
+            </div>
+            <ul className='container'>
+                {this.state.groceryList.map((item, index) => (
+                    <li key={index} onClick={this.crossedWord}>
+                        <img src={check} width="40px" alt="checkmark" />
+                        {item}
+                    </li>
+                ))}
+            </ul>
+            <div className='container'>
+                <button onClick={() => this.deleteItem()} className="btn btn-delete">Delete</button>
+            </div>
+            </form>
+            </div>
+        )
+    }
+}
